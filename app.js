00d4829dbcf3fc0f4da31da78f3bb13ea490307b
evblance <evblance@users.noreply.github.com>
2017-07-16 20:56:44 +1000

    Refactors
    - Tweaks UI and some styles
    - Changes infoWindow formatting and styles
    - Changes menu toggle functionality
    - Changes device-responsive behaviour
    - Adds Flickr AJAX request
    - Adds functionality to store existing downloaded request data to
    markers
    - Removes Google Streetview data request
    - Renames app
    - Changes global marker icon
    - Adds additional locations
    - Fixes a bug where zoom is too large when only selecting one marker
    - Adds and/or changes error handling for all asynchronous requests
    - Moves Bounds and InfoWindow services to VM
    - Removes option select interface for places selection
    - Adds list interface for places selection
    - Adds list filtering functionality
    - Adds further comments to app.js

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
-// TODO: Add OpenWeatherMap data for locality and display somewhere in UI
+// TODO: Add additional functionality
+//       - Add a UI for tourist to select their interests
+//          -- Locations array is then instead created based on the string and the array the geocoder returns
+//             for each interest. Example: User ticks only garden and filters out Montreal from the cities
+//             database -> placeId-1, placeId-2, ... then engage places service as currently implemented to
+//             create markers related to these interests in the city of choice.
+//          -- Callback to initMap will have to be removed from Google Maps API request script and instead
+//             called on above form submission.
 
+/* SUMMON JS SENSEI */
+'use strict';
+
+/* CONSTANTS */
+var MARKER_ICON = 'icons/marker.png';
+var NUM_PHOTOS = 10;
+
+var $placesList = $('.places-ui__list');
+
+// set a timeout for initial access to Google Maps API
+var mapsInitTimeout = window.setTimeout(function() {
+  window.alert('Error: App timed out on accessing Google Maps API.');
+}, 3000);
+
+
+/* GOOGLE MAPS MACHINERY */
+
+// This function initialises the map
 function initMap() {
 
-  MARKER_ICON = 'icons/marker.png';
+  // clear initialisation function callback call timeout
+  window.clearTimeout(mapsInitTimeout);
 
+  // hard-coded array of locations of interest
   var locations = [
-    {
-      lat: 45.500023,
-      lng: -73.566740
-    },
-    {
-      lat: 45.507933,
-      lng: -73.565259
-    },
-    {
-      lat: 45.508309,
-      lng: -73.566439
-    },
-    {
-      lat: 45.504813,
-      lng: -73.567877
-    },
-    {
-      lat: 45.503527,
-      lng: -73.564873
-    },
-
+    'Olympic Park Montreal',
+    'Mont-Royal Montreal',
+    'Vieux Montreal',
+    'Garden Montreal',
+    'Tower Montreal',
+    'Circuit Gilles-Villeneuve',
+    'Places des Arts Montreal',
+    'Notre-Dame Basilica',
+    'Chateau Dufresne',
+    'Universite de Montreal'
   ];
 
+  // array of custom map styles to remove default POIs from map
   var mapStyles = [
     {
       featureType: 'poi',
@@ -37,37 +54,44 @@ function initMap() {
     }
   ];
 
+  // initialise map setting the centre on downtown Montreal
   var montreal = {lat: 45.501631, lng: -73.567002};
-
   var nbhMap = new google.maps.Map(document.getElementById('map'),{
     center: montreal,
     zoom: 13,
+    maxZoom: 13,
     mapTypeControl: false,
     styles: mapStyles
   });
 
-  var infoWindow = new google.maps.InfoWindow();
-
+  // create Geocoder Service instance
   var geocoder = new google.maps.Geocoder;
 
+  // create Places Service instance
   var placesService = new google.maps.places.PlacesService(nbhMap);
 
-  var bounds = new google.maps.LatLngBounds();
-
-
+  // Function that initialises the markers
   function initMarkers() {
-    locations.forEach(function(location) {
-      createMarker(location.lat, location.lng);
-    });
+
+    var numLocs = locations.length;
+    var markerTimer = function(i) {
+      setTimeout(function() {
+        createMarker(locations[i], i);
+      }, 300 * i);
+    };
+    /* Creation of markers delayed by an appropriate interval so that
+     * that Geocoder query rates are not exceeded
+     */
+    for (var i = 0; i < numLocs; i++) {
+      markerTimer(i);
+    }
   }
 
-  // this function registers markers based on their name and coordinates
-  function createMarker(latitude, longitude) {
-
-    var coords = {lat: latitude, lng: longitude};
+  // Function that registers markers based on their location name
+  function createMarker(location, id) {
 
     var geocodeRequest = {
-      location: coords
+      address: location
     };
     geocoder.geocode(geocodeRequest, function(response, status) {
       if (status === 'OK') {
@@ -75,62 +99,45 @@ function initMap() {
         var placesRequest = {
           placeId: placeid
         };
+        // var coords = response[0].geometry.location;
+        var latitude = response[0].geometry.location.lat();
+        var longitude = response[0].geometry.location.lng();
+        var coords = {
+          lat: latitude,
+          lng: longitude
+        };
         placesService.getDetails(placesRequest, function(response, status) {
           if (status === google.maps.places.PlacesServiceStatus.OK) {
             var marker = new google.maps.Marker({
               name: response.name,
               position: coords,
-              map: nbhMap,
+              map: nbhVM.map,
               animation: google.maps.Animation.DROP,
-              icon: MARKER_ICON
+              icon: MARKER_ICON,
+              id: id,
+              images: [],
+              infoData: '',
+              address: response.formatted_address
             });
 
-            function markerListener(event) {
-              var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?key=AIzaSyBKC_eBqbyWzVOm2d8KEY5IC4NYxb2f4cA';
-              streetViewUrl += '&location=' + coords.lat + ',' + coords.lng;
-              streetViewUrl += '&size=200x200';
-
-              // push data into info window
-              var infoHtml = '<div id="loc-info"><h2 class="loc-heading">' + marker.name + '</h2><img class="loc-thumb" src="' + streetViewUrl + '" alt="location thumbnail"></div>';
-              infoWindow.close();
-              infoWindow.setContent(infoHtml);
-              infoWindow.open(nbhMap, marker);
-              infoWindow.addListener('closeclick', function() {
-                infoWindow.map = null;
-                infoWindow.marker = null;
-                nbhVM.unbounceAllMarkers();
-              });
-
-              // set only clicked marker to bounce
-              nbhVM.unbounceAllMarkers();
-              marker.setAnimation(google.maps.Animation.BOUNCE);
-
-              // center the map on the clicked marker
-              nbhMap.setCenter(coords);
-            }
-
-            marker.addListener('click', markerListener);
-            bounds.extend(coords);
-            nbhMap.fitBounds(bounds);
+            marker.addListener('click', nbhVM.selectMarker);
+            nbhVM.mapBounds.extend(coords);
+            nbhVM.map.fitBounds(nbhVM.mapBounds);
             nbhVM.markers.push(marker);
-            nbhVM.loadedMarkers.push(marker);
+            nbhVM.filteredMarkers.push(marker);
+            nbhVM.map.maxZoom = 25;
 
           } else {
-            window.alert('Places Services had an error');
+            window.alert('Places Services had an error', status, 'while retrieving data.');
           }
         });
 
       } else {
-        window.alert('Geocoder had an error');
+        window.alert('Geocoder had an error', status, 'while retrieving data.');
       }
     });
 
   }
-
-  nbhMap.addListener('click', function(event) {
-    infoWindow.close();
-    nbhVM.unbounceAllMarkers();
-  });
 
 
   /* VIEWMODEL */
@@ -139,48 +146,87 @@ function initMap() {
 
     var self = this;
 
+    // property that stores the Google Map instance
     this.map = nbhMap;
 
+    // property that stores a Bounds service instance
+    this.mapBounds = new google.maps.LatLngBounds();
+
+    // property that stores the infoWindow instance
+    this.infoWindow = new google.maps.InfoWindow();
+
+    // property that stores an array tracking the markers added to map
     this.markers = ko.observableArray([]);
 
-    this.loadedMarkers = ko.observableArray([]);
+    // property that tracks the filtering string entered by the user in the places UI
+    this.filterString = ko.observable('');
 
-    this.haveLoadedMarkers = ko.computed(function() {
-      return this.loadedMarkers().length > 0;
-    }, this);
+    // property that stores an array tracking the markers that are currently filtered
+    this.filteredMarkers = ko.observableArray([]);
 
-    this.unchooseMarker = function(data) {
-      var loadedMarkers = self.loadedMarkers();
-      var numLoadedMarkers = loadedMarkers.length;
-      for (var i = 0; i < numLoadedMarkers; i++) {
-        if (data.name === loadedMarkers[i].name) {
-          // hide marker
-          loadedMarkers[i].setMap(null);
-          // remove the marker from chosen array
-          self.loadedMarkers.splice(i,1);
-          return;
+    // method that helps monitor whether a marker is filtered
+    this.isfilteredMarker = ko.computed(function(data) {
+      for (var i = 0; i < self.filteredMarkers.length; i++) {
+        if (self.filteredMarkers[i].name === data.name ) {
+          return true;
         }
       }
+      return false;
+    });
+
+    /* This method filters the places list based on the string passed
+     * in a case insensitive manner
+     */
+    this.filterMarkers = function(string) {
+      $('.places-ui__list').find('.places-ui__list-item').removeClass('selected');
+      var reStr = new RegExp('(' + string.toLowerCase() + ')');
+      self.filteredMarkers([]);
+      return self.markers().filter(function(marker) {
+        var check = marker.name.toLowerCase();
+        var result = check.replace(reStr,'g');
+        var found = (check !== result);
+        if (found) {
+          self.filteredMarkers().push(marker);
+        }
+        return found;
+      });
     };
 
+    // locks map zoom to a maximum value
+    this.lockMapZoom = function() {
+      self.map.maxZoom = 18;
+    };
+
+    // unlock map zoom to fullest extent
+    this.unlockMapZoom = function() {
+      self.map.maxZoom = 25;
+    };
+
+    // This property tracks the selected place marker
+    this.selectedMarker = ko.observable();
+
+    /* This method helps ensure that only the selected place is highlighted
+     * in the places UI
+     */
+    this.getSelectedMarker = function(marker) {
+      if ( self.selectedMarker() ) {
+        return (self.selectedMarker().name === marker.name);
+      }
+      return false;
+    };
+
+    // method for updating certain marker-related things
     this.updateMarkers = function() {
       self.markers().forEach(function(marker) {
         marker.setMap(null);
+        self.infoWindow.close();
       });
-      console.log('hello');
-      self.loadedMarkers().forEach(function(marker) {
+      self.filteredMarkers().forEach(function(marker) {
         marker.setMap(self.map);
       });
     };
 
-    this.chooseMarker = function(data) {
-      self.markers().forEach(function(marker) {
-        if (marker.name === data.name) {
-          marker.setMap(self.map);
-        }
-      });
-    };
-
+    // method for removing bounce animation from all markers
     this.unbounceAllMarkers = function() {
       self.markers().forEach(function(marker) {
         if (marker.getAnimation() !== null) {
@@ -189,59 +235,174 @@ function initMap() {
       });
     };
 
-   // function to show a clicked marker
-   this.selectMarker = function(marker) {
-     // use the marker data to locate the correct marker form the ko arrays then update it to have the correct map
-    self.unbounceAllMarkers();
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    self.map.setCenter({ lat: marker.position.lat(), lng: marker.position.lng() });
-    self.markers().forEach(function(m) {
-      if (m.name === marker.name) {
-        m.setMap(self.map);
-      }
-    });
-   };
-
-   // function to check if marker is selected
-   this.chosenMarker = function(data) {
-     self.loadedMarkers().forEach(function(marker) {
-       if (marker === data) {
-         return true;
-       }
-     });
-     return false;
-   };
-
-    // function that recalculates map bounds to encompass only the currently selected places
+    // method that recalculates map bounds to encompass only the currently filtered places
     this.refreshBounds = function() {
-      var bounds = new google.maps.LatLngBounds();
-      self.loadedMarkers().forEach(function(marker) {
-        bounds.extend({lat: marker.position.lat(), lng: marker.position.lng()});
+      self.filteredMarkers().forEach(function(marker) {
+        self.mapBounds.extend({lat: marker.position.lat(), lng: marker.position.lng()});
       });
-      self.map.fitBounds(bounds);
+      self.map.fitBounds(self.mapBounds);
     };
 
-    this.placesListClick = function() {
+    // method that handles execution on a marker click
+    this.selectMarker = function() {
+
+      var marker = this;
+
+      // register the chosen marker
+      self.selectedMarker(marker);
+
+      // close previously open infoWindow
+      self.infoWindow.close();
+      self.infoWindow.map = null;
+
+      // if marker already has additional data loaded...
+      if (marker.infoData) {
+        // ...load that data and show infowindow
+        self.infoWindow.setContent(marker.infoData);
+        self.infoWindow.open(self.map, marker);
+      } else {
+        // otherwise show some basic information while further data is loading
+        var infoHTML = '';
+        infoHTML += '<div class="loc-info">';
+        infoHTML +=   '<h2 class="loc-heading">' + marker.name + '</h2>';
+        infoHTML += '<h3 class="loc-address">' + marker.address + '</h3>';
+        infoHTML +=   '<span class="loader"><i class="fa fa-2x fa-refresh fa-spin" aria-hidden="true"></i></span>';
+        infoHTML += '</div>';
+        marker.infoData = infoHTML;
+        self.infoWindow.setContent(marker.infoData);
+        self.infoWindow.open(self.map, marker);
+      }
+
+      // Only ask Flickr for images if no image urls already cached for this marker
+      if (marker.images.length === 0) {
+        var infoHTML = '';
+        // Build the request url starting from base
+        var flickrReqURL = 'https://api.flickr.com/services/rest/?api_key=4fbd721770420193baf71260f79454ac';
+        // set return format to json and make sure a pure json object is returned
+        flickrReqURL += '&format=json&nojsoncallback=1';
+        // set to search for photos
+        flickrReqURL += '&method=flickr.photos.search';
+        // make sure no more than NUM_PHOTOS is returned
+        flickrReqURL += '&per_page=' + NUM_PHOTOS;
+        // set coordinates for improved accuracy
+        flickrReqURL += '&lat=' + marker.position.lat();
+        flickrReqURL += '&lon=' + marker.position.lng();
+        // set to slightly better than city accuracy (11)
+        flickrReqURL += '&accuracy=12';
+        // return only photos
+        flickrReqURL += '&content_type=1';
+        // sorting by relevance
+        flickrReqURL += '&sort=relevance';
+        // pass the marker's name as the query text
+        flickrReqURL += '&text=' + marker.name;
+
+        // AJAX request for Flickr images related to this marker
+        $.ajax({
+          url: flickrReqURL,
+          type: 'GET',
+          datatype: 'json',
+          beforeSend: function() {
+
+            $('.loc-info').append('<span class="loader"><i class="fa fa-2x fa-refresh fa-spin" aria-hidden="true"></i></span>');
+
+          },
+          success: function(resp) {
+
+            var farmId, serverId, id, secret, title, photoURL, thumbURL, owner;
+            var infoHTML = '';
+            var photoArr = resp.photos.photo;
+            var numPhotos = photoArr.length;
+            for (var i = 0; i < numPhotos; i++) {
+              farmId = photoArr[i].farm;
+              serverId = photoArr[i].server;
+              id = photoArr[i].id;
+              secret = photoArr[i].secret;
+              owner = photoArr[i].owner;
+              thumbURL = 'https:\/\/farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '_t.jpg';
+              photoURL = 'https:\/\/www.flickr.com/photos/' + owner + '/' + id;
+              // add relevant photo urls to the marker's 'cache'
+              marker.images.push({
+                thumbURL: thumbURL,
+                photoURL: photoURL
+              });
+            }
+
+            // store additional data into the marker's information model
+            infoHTML += '<div class="loc-info">';
+            infoHTML += '<h2 class="loc-heading">' + marker.name + '</h2>';
+            infoHTML += '<h3 class="loc-address">' + marker.address + '</h3>';
+            infoHTML += '<ul class="loc-thumblist">';
+            marker.images.forEach(function(image) {
+              infoHTML += '<li class="loc-thumblist__item">';
+              infoHTML += '<a href="' + image.photoURL + '" target="_blank">';
+              infoHTML += '<img class="loc-thumb" src="' + image.thumbURL + '" alt="flickr img">';
+              infoHTML += '</a>';
+              infoHTML += '</li>';
+            });
+            infoHTML += '</ul>';
+            infoHTML += '</div>';
+            marker.infoData = infoHTML;
+
+          },
+          error: function(err) {
+
+            alert('Flickr API returned an error with state', err.readyState);
+
+          },
+          complete: function() {
+
+            // remove temporary loader icon and push new content to infoWindow
+            $('.loader').remove();
+            self.infoWindow.setContent(marker.infoData);
+
+          }
+        });
+      }
+
+      // push information from marker into infoWindow and show on map and marker
+      self.infoWindow.setContent(marker.infoData);
+      self.infoWindow.open(self.map, marker);
+
+      // add 'closeclick' listener on the infoWindow
+      self.infoWindow.addListener('closeclick', function() {
+        self.infoWindow.map = null;
+        self.infoWindow.marker = null;
+        self.unbounceAllMarkers();
+        $placesList.find('.places-ui__list-item').removeClass('selected');
+      });
+
+      // set only clicked marker to bounce
+      self.unbounceAllMarkers();
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+
+      // center the map on the clicked marker
+      self.map.setCenter(marker.position);
+    };
+
+    // perform refresh of markers and bounds on map
+    this.refreshMarkerDisplay = function(data) {
       self.updateMarkers();
       self.refreshBounds();
     };
 
   };
 
-
+  // initialise ViewModel and bind it
   var nbhVM = new NeighbourhoodViewModel();
   ko.applyBindings(nbhVM);
 
+  // initialise markers
   initMarkers();
+
 }
 
-// check visibility instead of toggling to avoid bug on user resize
-$('.hamburger').on('click', 'span', function() {
-  if ( $('form').is(':visible') ) {
-    $('form').removeClass('ui-shown');
-    $('.hamburger').removeClass('hamburger-on');
+// handle click events on menu toggle
+$('.app__menu').on('click', '.app__menu-toggle', function() {
+  if ( $('.places-ui').hasClass('places-ui--shown') ) {
+    $('.places-ui').removeClass('places-ui--shown');
+    $('.app__menu-toggle').attr('icon', '☰');
   } else {
-    $('.hamburger').addClass('hamburger-on');
-    $('form').addClass('ui-shown');
+    $('.places-ui').addClass('places-ui--shown');
+    $('.app__menu-toggle').attr('icon', '❌');
   }
 });
